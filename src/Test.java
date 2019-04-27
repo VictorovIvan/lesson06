520816e005828f2cc02a70173b54f34c9f1796ed
VictorovIvan <victorov.i.vl@outlook.com>
2019-04-28 00:56:52 +0300

    Added project

---
+++ src/Test.java
@@ -0,0 +1,39 @@
+import task01.ReadTextFile;
+import task02.GeneratorTextFile;
+
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * First task
+         */
+        System.out.println("Первое задание:");
+        String pathTextFile = ".\\taskOneExample\\";
+        String nameTextFile = "Five ways you could become a memory champion.txt";
+        ReadTextFile readTextFile = new ReadTextFile();
+
+        try {
+            readTextFile.readTextFileAndCreateNew(pathTextFile, nameTextFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Second task
+         */
+        System.out.println("Второе задание:");
+        GeneratorTextFile generatorTextFile = new GeneratorTextFile();
+        String somePath = ".\\taskTwoExample\\";
+        int someN = 5;
+        int someSize = 3;
+        String[] someWords = new String[]{"Cat", "Dog", "Horse"};
+        int someProbability = 50;
+
+        try {
+            generatorTextFile.getFiles(somePath, someN, someSize, someWords, someProbability);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
